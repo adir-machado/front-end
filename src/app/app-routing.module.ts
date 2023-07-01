e46471bcd61cdf29ff7b8bd5b95f7496378462b3
Paulo Daniel <paulo.daniel@souunit.com.br>
2023-07-01 16:26:19 -0300

    Add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandPageComponent } from './views/land-page/land-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
